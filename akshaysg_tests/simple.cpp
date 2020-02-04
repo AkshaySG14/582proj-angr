f11c0fe4c2ed4689d142ce7ef62ce6ee3e58f1c8
akshaysg <akshaysg@umich.edu>
2020-02-04 18:48:28 -0500

    Added simple test

---
+++ akshaysg_tests/simple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::endl;
+using std::cout;
+
+static int boo = 5, goo = 19, doo = 21;
+
+int main(int argc, char **argv)
+{
+    
+    cout << "Hello World" << endl;
+    return 0;
+}

014772a2c89f74d89b05a0a47c5da572b0ad5fe6
akshaysg <akshaysg@umich.edu>
2020-02-04 22:35:00 -0500

    Minor changes

--- akshaysg_tests/simple.c
+++ akshaysg_tests/simple.c
@@ -6,14 +6,16 @@ static int boo = 5, goo = 19, doo = 21;
 int main(int argc, char **argv)
 {
     printf("Hello World\n");
+    int arr[10];
     int test = atoi(argv[1]);
     int length = atoi(argv[2]);
     if (argc < 2) {
         printf("Error\n");
         return 0;
     }
-    if (test > 0) {
+    if (test > 0 && length <= 10) {
         for (int i = 0; i < length; ++i) {
+            arr[i] = 5;
             printf("%d\n", i);
         }
         printf("Test");

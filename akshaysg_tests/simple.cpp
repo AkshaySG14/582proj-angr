0a5ee86d626c1a6df1e53ea7d412d2a194b383b3
akshaysg <akshaysg@umich.edu>
2020-02-04 21:56:28 -0500

    Modified tests

--- akshaysg_tests/simple.cpp
+++ akshaysg_tests/simple.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using std::endl;
 using std::cout;
@@ -9,5 +10,19 @@ int main(int argc, char **argv)
 {
     
     cout << "Hello World" << endl;
+    int test = std::stoi(argv[1]);
+    int length = std::stoi(argv[2]);
+    if (argc < 2) {
+        cout << "Error" << endl;
+        exit(1);
+    }
+    if (test > 0) {
+        for (int i = 0; i < length; ++i) {
+            cout << i << endl;
+        }
+        cout << "Test" << endl;
+    } else {
+        cout << "Second Branch" << endl;
+    }
     return 0;
 }

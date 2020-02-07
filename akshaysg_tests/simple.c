c92e2cc9f90e17873046c341198fc42548456fd3
akshaysg <akshaysg@umich.edu>
2020-02-06 19:03:25 -0500

    Changed simple test

--- akshaysg_tests/simple.c
+++ akshaysg_tests/simple.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 static int boo = 5, goo = 19, doo = 21;
 
 int main(int argc, char **argv)
 {
+    time_t t;
+    srand((unsigned) time(&t));
+
     printf("Hello World\n");
-    int arr[100];
-    for (int i = 0; i < 100; ++i) {
-        arr[i] = i;
-    }
-    int test = atoi(argv[1]);
-    int length = atoi(argv[2]);
-    if (argc < 2) {
-        printf("Error\n");
-        return 0;
-    }
-    else {
-        if (test > 0) {
-            if (length <= 10) {
-                printf("%d\n", arr[length]);
-                printf("%d\n", arr[test]);
-                for (int i = 0; i < length; ++i) {
-                    printf("%d\n", arr[i] = 5);
-                    printf("%d\n", i);
-                }
+    int test = rand() % 20;
+    int length = rand() % 20;
+    if (test > 0) {
+        if (length <= 10) {
+            for (int i = 0; i < length; ++i) {
+                printf("%d\n", i);
             }
-            printf("Test");
-        } else {
-            printf("Second Branch");
         }
+        printf("Test\n");
+    } else {
+        printf("Second Branch\n");
     }
     return 0;
 }
